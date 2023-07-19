b88906702c3c0ff690b65325f35f6c814c5750a0
developerekene <developerekene@gmail.com>
2023-07-19 11:42:01 +0100

    Basic set up done

---
+++ src/App/Constants/Assets.js
@@ -0,0 +1,5 @@
+export const Assets = {
+  images: {},
+  sizes: {},
+  colors: {},
+};
---
+++ src/App/UI/AppEntry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppEntry = () => {
+  return (
+    <View>
+      <Text>This is where the main app Begins</Text>
+    </View>
+  )
+}
+
+export default AppEntry
