df76ffdbe6cb829c2ab3edb6b7469e119eb49f83
David Adisa <adisad377@gmail.com>
2023-07-20 13:47:47 +0100

    David- David's initial commit

--- src/App/UI/Components/Navbar/Navbar.js
+++ src/App/UI/Components/Navbar/Navbar.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {  useState } from 'react';
 import "../Navbar/Navbar.css";
 import TextApp from '../Text/Text';
+import {navLinks} from '../../../Constants/data'
+import { HiMenu, HiX } from "react-icons/hi";
+import iLinkLogo from "../../../Image/svg/IlinkLogo-darkmode.svg"
 
 const Navbar = () => {
+  const [openNav, setOpenNav] = useState(false)
   return (
-    <div>
-      <TextApp inputText={"This is the text!"}/>
+    <div className='nav-item-container'>
+      <img src={iLinkLogo} height={"60px"} alt="iLinkLogo" />
+      <div className="nav-links">
+        {
+          navLinks.map((item)=>{
+            return(
+              <TextApp inputText={item.title} key={item.title}/>
+            )
+          })
+        }
+      </div>
+      <div className={!openNav? "openIcon" : "closeIcon"} onClick={()=>{setOpenNav(!openNav)}}>
+        { !openNav ? <HiMenu /> : <HiX />}
+      </div>
+      <div className={openNav ? "open-navbar" : "close-navbar"}>
+        {
+          navLinks.map((item)=>{
+            return(
+              <TextApp inputText={item.title} key={item.title}/>
+            )
+          })
+        } 
+      </div>
     </div>
   )
 }
