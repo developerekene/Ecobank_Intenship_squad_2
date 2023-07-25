9e521d22998da257fe4f911df97437f9df8e460f
Kinako Kiaka <kinakogodswill@gmail.com>
2023-07-25 08:50:39 +0100

    kk's initial commit

--- src/App/UI/Components/Footer/Footer.js
+++ src/App/UI/Components/Footer/Footer.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import "../Footer/Footer.css";
+import React from "react";
+import '.Footer.css/';
+// import TextApp from "../Text/Text";
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+
+const Footer=()=>{
+    return(
+    <div className="Footer">
+        <div className="sb__Footer section_padding">
+         <div className="sb__Footer-links"> 
+            <div  className="sb__Footer-links-div">
+                <h4>For Business</h4> 
+                <a href=""></a>
+                <p> Partnership</p>
+
+                <a href=""></a>
+                <p>Advertise</p>
+            </div> 
+
+            <div className="sb__Footer-links-div">
+               <h4>Company</h4>
+               <a href=""></a>
+               <p>Developers</p>
+
+               <a href=""></a>
+               <p>About</p> 
+               
+               <a href=""></a>
+               <p>Contact</p>
+                </div>      
+         </div>
+
+         <div className="sb__Footer-links-div">
+            <h4>Partners</h4>
+            <a>Ecobank Nigeria</a>
+            <a>Eprocess</a>
+            <a>Google</a>
+         </div>
+
+         <hr></hr>
+
+         <div className="sb__Footer-below">
+            <div className="sb__Footer-copyright">
+                <p>
+                    @{new Date().getFullYear()} codeInn. All right reserved
+                </p>
+            </div>
+            <div className="sb__Footer-below-links">
+                <a href><p>Privacy</p></a>
+            </div>
+         </div>
+
+
+
+        </div>
+    </div>
+    )
 }
-
-export default Footer
+ export default Footer;
