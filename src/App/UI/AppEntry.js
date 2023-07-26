179171537c18d18b0cc62234c0e0385c63dc14a9
Afolabi Praise <ifepraise2004@gmail.com>
2023-07-26 21:22:28 +0100

    Button component completed

--- src/App/UI/AppEntry.js
+++ src/App/UI/AppEntry.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import Button from './Components/button/Button';
 
 const AppEntry = () => {
   return (
     <>
-    <Navbar />
-    <Footer />
+      <Navbar />
+      <Button text={'Get started'} />
+      <Footer />
+
     </>
   )
 }
---
+++ src/App/UI/Components/button/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Button.css';
+
+
+function Button({ text }) {
+    return (
+        <div>
+            <button className="button">{text}</button>
+        </div>
+    )
+}
+
+export default Button;
