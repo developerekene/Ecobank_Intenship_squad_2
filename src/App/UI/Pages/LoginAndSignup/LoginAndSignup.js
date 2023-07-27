b570e15690043b3829f9cb6af3a92ae1c1311db5
David Adisa <adisad377@gmail.com>
2023-07-27 11:19:19 +0100

    David's Adjustment

--- src/App/UI/Pages/LoginAndSignup/LoginAndSignup.js
+++ src/App/UI/Pages/LoginAndSignup/LoginAndSignup.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Signup from "./signup/Signup";
 import Login from './login/Login';
 import Navbar from '../../Components/Navbar/Navbar';
-import Footer from '../../Components/Footer/Footer'
 
 const LoginAndSignup = () => {
   const [currentForm, setCurrentForm] = useState("Login");
