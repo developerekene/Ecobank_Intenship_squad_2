885028753811cba27991b8986ab5914272c0e3d5
David Adisa <adisad377@gmail.com>
2023-07-30 22:49:47 +0100

    a little adjustment in the code and Browse oppotunity in progress.

--- src/App/UI/Pages/BrowseOpportunity/BrowseOpportunity.js
+++ src/App/UI/Pages/BrowseOpportunity/BrowseOpportunity.js
@@ -2,18 +2,17 @@ import React from 'react'
 import "./BrowseOpportunity.css"
 import OpportunityCard from './opportunityCard/OpportunityCard'
 import Sidebar from './sidebar/Sidebar'
+import SideBarBackup from './sidebar_backup/SideBarBackup'
 
 function BrowseOpportunity() {
+
   return (
     <div className='BrowseOpportunity'>
       <Sidebar />
+      <SideBarBackup />
       <div className='result-display'>
         <p className='search-result'>{"searchResult"}</p>
         <hr />
-            <OpportunityCard companyLogo={""} opportunityType={"Internship"} jobTitle={"Frontend Developer"}
-            companyName={"Ecobank Africa"} NatureOfJob={"Onsite"} TimePosted={"1year ago"}/>
-            <OpportunityCard companyLogo={""} opportunityType={"Internship"} jobTitle={"Frontend Developer"}
-            companyName={"Ecobank Africa"} NatureOfJob={"Onsite"} TimePosted={"1year ago"}/>
             <OpportunityCard companyLogo={""} opportunityType={"Internship"} jobTitle={"Frontend Developer"}
             companyName={"Ecobank Africa"} NatureOfJob={"Onsite"} TimePosted={"1year ago"}/>
             <OpportunityCard companyLogo={""} opportunityType={"Internship"} jobTitle={"Frontend Developer"}
