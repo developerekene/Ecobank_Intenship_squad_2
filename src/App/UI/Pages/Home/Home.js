6baf1a6882ccfc85ce57e2050b8d62ce02abb1e6
Kinako Kiaka <kinakogodswill@gmail.com>
2023-08-01 10:39:51 +0100

    Pulled from develop

--- src/App/UI/Pages/Home/Home.js
+++ src/App/UI/Pages/Home/Home.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import "../Home/Home.css";
+import man from  "../../../Image/png/man.png";
+import lady from  "../../../Image/png/lady.png";
+import man2 from  "../../../Image/png/man2.png";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section class="container">
+      <div class="welcome">
+        <div class="welcome_image"></div>
+        <h2>Title</h2>
+        <p>text</p>
+      </div>
+      <div class="card">
+            <div class="cards">
+              <div class="card_image">
+                <h2>Title</h2>
+                <p>text</p>
+              </div>
+            </div>
+
+            <div class="cards">
+              <div class="card_image">
+                <h2>Title</h2>
+                <p>text</p>
+              </div>
+            </div>
+
+            <div class="cards">
+              <div class="card_image">
+                <h2>Title</h2>
+                <p>text</p>
+              </div>
+            </div>
+          </div>        
+    </section>
   )
 }
 
