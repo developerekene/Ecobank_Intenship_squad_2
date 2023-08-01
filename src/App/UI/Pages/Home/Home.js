863452a838a78e1093cb3fd6d32f3555c8df46ea
Kinako Kiaka <kinakogodswill@gmail.com>
2023-08-01 15:42:44 +0100

    home page update

--- src/App/UI/Pages/Home/Home.js
+++ src/App/UI/Pages/Home/Home.js
@@ -3,35 +3,50 @@ import "../Home/Home.css";
 import man from  "../../../Image/png/man.png";
 import lady from  "../../../Image/png/lady.png";
 import man2 from  "../../../Image/png/man2.png";
+import stats2 from "../../../Image/png/stats2.png"
+import Button from '../../Components/button/Button';
+
+
+
 
 const Home = () => {
   return (
     <section class="container">
       <div class="welcome">
-        <div class="welcome_image"></div>
-        <h2>Title</h2>
-        <p>text</p>
+        <div className="welcome_text_button">
+          <h2>Connecting Talent to Companies</h2>
+          <p>Providing you with the opportunity to maximize your abilities to the fullest</p>
+          <Button text={"Learn more"} />
+        </div>
+        <div class="welcome_image">
+          <img src={lady} ></img>
+        </div>
       </div>
       <div class="card">
             <div class="cards">
+              <div className='card_text'>
+                <h3>Partnership</h3>
+                <p>Work with us</p>
+              </div>
+              <div class="card_image"></div>
+            </div>
+
+            <div class="cards">
+            <div className='card_text'>
+                <h3>Our Impact</h3>
+                <Button text={"Reviews"} />
+              </div>
               <div class="card_image">
-                <h2>Title</h2>
-                <p>text</p>
+                <img src={stats2}></img>
               </div>
             </div>
 
             <div class="cards">
-              <div class="card_image">
-                <h2>Title</h2>
-                <p>text</p>
+              <div class="card_text">
+                <h3>Upload or Update your CV</h3>
+                <p></p>
               </div>
-            </div>
-
-            <div class="cards">
-              <div class="card_image">
-                <h2>Title</h2>
-                <p>text</p>
-              </div>
+              <div class="card_image"></div>
             </div>
           </div>        
     </section>
