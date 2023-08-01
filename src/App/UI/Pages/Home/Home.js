39da0a3af895ab33011afacf1e6ae74ca3329bc3
David Adisa <adisad377@gmail.com>
2023-08-01 14:21:14 +0100

    Revert "Merge pull request #4 from developerekene/ife_dev"
    This reverts commit f704314c165bbb0e3cc5dc2a42c4f7cedf68d2d9, reversing
    changes made to d4eb25053d907bd306123df5fd048874c4f31799.

--- src/App/UI/Pages/Home/Home.js
+++ src/App/UI/Pages/Home/Home.js
@@ -1,40 +1,9 @@
 import React from 'react';
 import "../Home/Home.css";
-import man from  "../../../Image/png/man.png";
-import lady from  "../../../Image/png/lady.png";
-import man2 from  "../../../Image/png/man2.png";
 
 const Home = () => {
   return (
-    <section class="container">
-      <div class="welcome">
-        <div class="welcome_image"></div>
-        <h2>Title</h2>
-        <p>text</p>
-      </div>
-      <div class="card">
-            <div class="cards">
-              <div class="card_image">
-                <h2>Title</h2>
-                <p>text</p>
-              </div>
-            </div>
-
-            <div class="cards">
-              <div class="card_image">
-                <h2>Title</h2>
-                <p>text</p>
-              </div>
-            </div>
-
-            <div class="cards">
-              <div class="card_image">
-                <h2>Title</h2>
-                <p>text</p>
-              </div>
-            </div>
-          </div>        
-    </section>
+    <div>Home</div>
   )
 }
 
