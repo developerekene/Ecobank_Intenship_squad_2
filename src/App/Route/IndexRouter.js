ba22c6dc2a35fc812b18c90e5fb1c68f1261f5c8
Afolabi Praise <ifepraise2004@gmail.com>
2023-08-02 10:48:01 +0100

    "Profile Page in progress"

--- src/App/Route/IndexRouter.js
+++ src/App/Route/IndexRouter.js
@@ -2,18 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BrowseOpportunity from "../UI/Pages/BrowseOpportunity/BrowseOpportunity";
 import Home from '../UI/Pages/Home/Home'
-import Profile from '../UI/Pages/Profile/Profile';
-import LoginAndSignup from "../UI/Pages/LoginAndSignup/LoginAndSignup";
-
+import Profile from "../UI/Pages/Profile/Profile";
 
 const index = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route Component={LoginAndSignup} path={"/"}></Route>
-        <Route Component={Home} path={"/home"}></Route>
-        <Route Component={BrowseOpportunity} path={"/opportunity"}></Route>
-        <Route Component={Profile} path={"/profile"}></Route>
+        {/* <Route Component={Home} path={"/"}></Route> */}
+        <Route Component={BrowseOpportunity} path={"/browse"}></Route>
+        <Route Component={Profile} path={"/"}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/App/UI/Pages/Profile/Profile.js
+++ src/App/UI/Pages/Profile/Profile.js
@@ -1,16 +1,71 @@
 import React from 'react';
-import "../Profile/Profile.css";
 import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
+import { Assets } from '../../../Constants/Assets';
+import "./Profile.css"
+import Button from "../../Components/button/NavigateButton/NavigateButton";
 
 const Profile = () => {
   return (
     <>
       <Navbar />
-      <div>Profile</div>
+      <div className="Profile">
+        <div className="upper-container">
+          <div className="col-md-4">
+            <div className="image-container">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsQKVgKb7G1p4Y-l2k9Vk9VSy5rVixd4D8mQ&usqp=CAU" alt='' height="100px" width="100px" />
+            </div>
+
+          </div>
+          <div className="col-md-2">
+            <button type="submit" class="move-right">Edit Profile</button>
+          </div>
+
+
+        </div>
+        <div className="col-md-6">
+          <div className="middle-container">
+            <h5>Stephen Curry</h5>
+            <h6>Frontend web developer</h6>
+            <p className="profile-rating mt-3 mb-5">RANKINGS: <span> 6/10 </span></p>
+
+
+            <ul className="nav nav-tabs" role="tablist">
+              <li className="nav-item">
+                <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab">Timeline</a>
+              </li>
+            </ul>
+
+          </div>
+
+
+
+        </div>
+
+        <div className="lower-container">
+          {/* left side url */}
+          <div className="col-md-4">
+            <div className="profile-work">
+              <p>WORK LINK</p>
+              <a>Youtube</a> <br />
+              <a>Instagram</a> <br />
+              <a>Github</a> <br />
+              <a>Figma</a> <br />
+              <a>Software Engineer</a> <br />
+            </div>
+          </div>
+          {/* right side url */}
+        </div>
+      </div>
       <Footer />
     </>
+
   )
 }
+
+
 
 export default Profile
