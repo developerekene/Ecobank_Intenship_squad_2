a6854408461d59aab4e488f369c8dde04792001b
David Adisa <adisad377@gmail.com>
2023-08-02 10:53:53 +0100

    fixed the navigation paths

--- src/App/Route/IndexRouter.js
+++ src/App/Route/IndexRouter.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BrowseOpportunity from "../UI/Pages/BrowseOpportunity/BrowseOpportunity";
 import Home from '../UI/Pages/Home/Home'
 import Profile from "../UI/Pages/Profile/Profile";
+import LoginAndSignup from "../UI/Pages/LoginAndSignup/LoginAndSignup";
 
 const index = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route Component={Home} path={"/"}></Route> */}
-        <Route Component={BrowseOpportunity} path={"/browse"}></Route>
-        <Route Component={Profile} path={"/"}></Route>
+        <Route Component={LoginAndSignup} path={"/"}></Route>
+        <Route Component={Home} path={"/home"}></Route>
+        <Route Component={BrowseOpportunity} path={"/opportunity"}></Route>
+        <Route Component={Profile} path={"/profile"}></Route>
       </Routes>
     </BrowserRouter>
   );
