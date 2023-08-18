cbe7eb3c0898e26ac4034204dcf11586c2c230d5
Afolabi Praise <ifepraise2004@gmail.com>
2023-08-18 11:46:12 +0100

    profile update

--- src/App/UI/Components/Navbar/Navbar.js
+++ src/App/UI/Components/Navbar/Navbar.js
@@ -1,84 +1,83 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import "../Navbar/Navbar.css";
 import TextApp from '../Text/Text';
-import {navLinks} from '../../../Constants/data'
+import { navLinks } from '../../../Constants/data'
 import { HiMenu, HiX } from "react-icons/hi";
 import { Assets } from '../../../Constants/Assets';
 import Button from "../button/NavigateButton/NavigateButton"
-import Andres from "../../../Image/png/Andres.png"
 
 const Navbar = () => {
-  
+
   const [openNav, setOpenNav] = useState(false)
-const [loggedIn, setloggedIn] = useState(true)
+  const [loggedIn, setloggedIn] = useState(true)
 
   return (
     <div className='navbar'>
-        <div className='nav-item-container'>
-          <a href={"/home"}>
-            <img src={Assets.images['logo-lightmode']} height={"60px"} alt="iLinkLogo" />
-          </a>
-           
-          <div className="nav-links">
-            {
-                navLinks.map((item)=>{
-                  return(
-                    <div className="nav-link">
-                      {
-                      <a href={item.href}>
-                        <TextApp inputText={item.title} key={item.title}/>
-                      </a>
-                      }
-                    </div>
-                  )
-                })
-              }
-              </div>
-              
+      <div className='nav-item-container'>
+        <a href={"/home"}>
+          <img src={Assets.images['logo-lightmode']} height={"60px"} alt="iLinkLogo" />
+        </a>
 
-          <div className='navButtons'>
-            {
-              
-                loggedIn ?
-                <>
-                  <a href={"/profile"}>
-                    <img className='profile-picture' src={Andres} alt="profile" />
-                  </a>
-                </>
-                
-                :
-                <Button text={'Login'} />
-            }
-            
+        <div className="nav-links">
+          {
+            navLinks.map((item) => {
+              return (
+                <div className="nav-link">
+                  {
+                    <a href={item.href}>
+                      <TextApp inputText={item.title} key={item.title} />
+                    </a>
+                  }
+                </div>
+              )
+            })
+          }
         </div>
 
-        <div className="openIcon" onClick={()=>{setOpenNav(!openNav)}}>
+
+        <div className='navButtons'>
+          {
+
+            loggedIn ?
+              <>
+                <a href={"/profile"}>
+                  <img className='profile-picture' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsQKVgKb7G1p4Y-l2k9Vk9VSy5rVixd4D8mQ&usqp=CAU" />
+                </a>
+              </>
+
+              :
+              <Button text={'Login'} />
+          }
+
+        </div>
+
+        <div className="openIcon" onClick={() => { setOpenNav(!openNav) }}>
           <HiMenu />
         </div>
         <div className={openNav ? "open-navbar" : "close-navbar"}>
-        <div className="closeIcon" onClick={()=>{setOpenNav(!openNav)}}>
-           <HiX />
-        </div>
-        
+          <div className="closeIcon" onClick={() => { setOpenNav(!openNav) }}>
+            <HiX />
+          </div>
+
           {
-            navLinks.map((item)=>{
-              return(
+            navLinks.map((item) => {
+              return (
                 <>
                   <a href={item.href}>
-                    <TextApp inputText={item.title} key={item.title}/>
+                    <TextApp inputText={item.title} key={item.title} />
                   </a>
                 </>
-                
+
               )
             })
-          } 
+          }
           <div className='navButtons-side'>
             <Button text={'Login'} />
           </div>
         </div>
       </div>
     </div>
-    
+
   )
 }
 
